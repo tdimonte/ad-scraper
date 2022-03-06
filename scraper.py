3ffc6d836eecdde9e7e6c5be7e097118d7c71e01
Tom DiMonte <tdimonte@luc.edu>
2022-03-05 21:25:54 -0600

    web access test

---
+++ scraper.py
@@ -0,0 +1,5 @@
+#Selenium imports
+from selenium import webdriver
+
+driver = webdriver.Chrome('C:/Users/tadim/OneDrive/Desktop/dev/projects/dependencies/chromedriver_win32/chromedriver.exe')
+driver.get('https://www.instagram.com/')
