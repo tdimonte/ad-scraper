09c0d5164256e31c2e27213e0723fe94d44e2144
Tom DiMonte <tdimonte@luc.edu>
2022-03-07 23:28:57 -0600

    Added functional log-in, dependencies

--- scraper.py
+++ scraper.py
@@ -1,5 +1,14 @@
 #Selenium imports
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome('C:/Users/tadim/OneDrive/Desktop/dev/projects/dependencies/chromedriver_win32/chromedriver.exe')
 driver.get('https://www.instagram.com/')
+
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+username.send_keys('SelScraper')
+username.send_keys('SeleniumScraper3')
