1950a6b689a45dbe9f57d2963320476c69eede04
Tom DiMonte <tdimonte@luc.edu>
2022-03-09 13:23:35 -0600

    Seperated Facebook and Instagram modules, added imports

---
+++ fb.py
@@ -0,0 +1,5 @@
+#Selenium imports
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
