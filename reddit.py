9eaccc13dc0d6cd8418a5809cde0759dba3ae27a
Tom DiMonte <tdimonte@luc.edu>
2022-03-11 10:28:34 -0600

    Added reddit.py, login for reddit.py

---
+++ reddit.py
@@ -0,0 +1,20 @@
+#Selenium imports
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome('C:/Users/tadim/OneDrive/Desktop/dev/projects/dependencies/chromedriver_win32/chromedriver.exe')
+driver.get('https://www.reddit.com/login/')
+
+#Reddit access
+username = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
+password = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
+
+username.clear()
+password.clear()
+
+username.send_keys('SelScraper')
+password.send_keys('SeleniumScraper3')
+login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
+
