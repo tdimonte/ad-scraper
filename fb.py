77a0651bc7181c367116c3c24c8f7e81fb7ef1bc
Tom DiMonte <tdimonte@luc.edu>
2022-03-13 17:27:28 -0500

    Updated fb.py, Added import keys, search

--- fb.py
+++ fb.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome('C:/Users/tadim/OneDrive/Desktop/dev/projects/dependencies/chromedriver_win32/chromedriver.exe')
 driver.get('https://www.facebook.com/')
@@ -18,4 +19,12 @@ email.send_keys('selscraper@gmail.com')
 password.send_keys('SeleniumScraper3')
 login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']"))).click()
 
-home = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[aria-label='Home']"))).click()
+#Facebook Search
+#home = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[aria-label='Home']"))).click()
+search_logo = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "label[class='lzcic4wl gs1a9yip br7hx15l h2jyy9rg n3ddgdk9 owxd89k7 rq0escxv j83agx80 a5nuqjux l9j0dhe7 k4urcfbm kbf60n1y b3i9ofy5']"))).click()
+search = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[aria-label='Search Facebook']")))
+
+search.clear()
+keyword = "50% Pets"
+search.send_keys(keyword)
+search.send_keys(Keys.ENTER)
